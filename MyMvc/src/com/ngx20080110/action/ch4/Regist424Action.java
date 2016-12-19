328c7cdba0f8f9b1f4e1f5d5440cc50934bd1b68
Guan Ying WU <gy.wu@macaowater.com>
2016-12-19 12:22:06 +0800

    vistor validation

---
+++ MyMvc/src/com/ngx20080110/action/ch4/Regist424Action.java
@@ -0,0 +1,18 @@
+package com.ngx20080110.action.ch4;
+
+import com.ngx20080110.bean.User424;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class Regist424Action extends ActionSupport {
+
+	private User424 user;
+
+	public User424 getUser() {
+		return user;
+	}
+
+	public void setUser(User424 user) {
+		this.user = user;
+	}
+	
+}
