9eda5cc39cbf7e43db48a72bf342ffa566c67a0f
Guan Ying WU <gy.wu@macaowater.com>
2016-12-28 11:10:31 +0800

    Spring: constructor injection

--- MyMvc/src/com/ngx20080110/ch7/service/Chinese.java
+++ MyMvc/src/com/ngx20080110/ch7/service/Chinese.java
@@ -2,10 +2,8 @@ package com.ngx20080110.ch7.service;
 
 public class Chinese implements Person {
 	private Axe axe;
-	public Axe getAxe() {
-		return axe;
-	}
-	public void setAxe(Axe axe) {
+	public Chinese() {}
+	public Chinese(Axe axe) {
 		this.axe = axe;
 	}
 	@Override
